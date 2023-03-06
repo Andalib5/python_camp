3fa8cd529901b6c57ab2e1bd220387d01cb36934
Andalib5 <106279394+Andalib5@users.noreply.github.com>
2023-03-06 11:47:21 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,11 +1,6 @@
 # 6-  Liste elemanlarını ters çevirin.
 names = ['Ali','Yağmur','Hakan','Deniz']
 
-newlist = []
-
-for i in range(-1, -len(names)-1, -1):
-    newlist.append(names[i])
-
-names = newlist
+names.reverse()
 
 print(names)
